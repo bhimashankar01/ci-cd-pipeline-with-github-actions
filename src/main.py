f1ed00796a8341d228021201d1fac2642ea9fbb9
Bhimashankar <bhimawadkar86@gmail.com>
2026-05-15 15:51:07 +0530

    installed required libraries before running

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+"""Simple FastAPI application."""
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,9 +7,11 @@ app = FastAPI()
 
 @app.get("/")
 def home():
+    """Return welcome message."""
     return {"message": "Hello FastAPI"}
 
 
 @app.get("/add")
 def add(a: int, b: int):
+    """Return addition of two numbers."""
     return {"result": a + b}
